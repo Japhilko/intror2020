47345a362be1862447d7257d73813969fa8a3c43
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2020-03-09 07:38:47 +0100

    incourse1

--- code/incourse1.R
+++ code/incourse1.R
@@ -29,3 +29,12 @@ K = 5
 h = 0.25
 
 (Q = sqrt(2*D*K/h))
+
+
+
+# Exercise: Vectors and Indexing ------------------------------------------
+
+height <- c(1.80, 1.65, 1.60, 1.93)
+weight <- c(87, 58, 65, 100)
+
+weight/height^2
