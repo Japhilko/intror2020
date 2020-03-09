ee5640783c21a6c1164e24b15600849f5fcf3c73
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2020-03-09 21:38:42 +0100

    data processing

--- code/incourse1.R
+++ code/incourse1.R
@@ -3,7 +3,7 @@
 
 
 
-# Getting started ---------------------------------------------------------
+# ch: Getting started ---------------------------------------------------------
 
 
 
@@ -22,7 +22,7 @@
 
 
 
-# Economic Order Quantity Model -------------------------------------------
+# Exercise: Economic Order Quantity Model -------------------------------------------
 
 D = 1000
 K = 5
@@ -32,14 +32,59 @@ h = 0.25
 
 
 
-# Exercise: Vectors and Indexing ------------------------------------------
-
-height <- c(1.80, 1.65, 1.60, 1.93)
-weight <- c(87, 58, 65, 100)
-
-weight/height^2
+# ch: Data Processing ---------------------------------------------------------
 
 
+
+# Ames Housing data -------------------------------------------------------
+
+
+
+ames_df <- AmesHousing::make_ames()
+
+
+# Exercise: Compute the price per area ------------------------------------
+
+price_area <- ames_df$Lot_Area/ames_df$Sale_Price
+max(price_area)
+
+
+# Exercise: Missing values ------------------------------------------------
+
+data("airquality")
+
+# How many missing values are in the built-in data set airquality?
+
+sum(is.na(airquality))
+
+
+# Which variables are the missing values concentrated in?
+
+sum(is.na(airquality$Ozone))
+sum(is.na(airquality$Solar.R))
+sum(is.na(airquality$Wind))
+sum(is.na(airquality$Temp))
+sum(is.na(airquality$Month))
+sum(is.na(airquality$Day))
+
+
+# How would you impute the mean or median for these values?
+
+airquality$Ozone[is.na(airquality$Day)] <- mean(airquality$Day,na.rm=T)
+
+
+# How would you omit all rows containing missing values?
+
+airq <- na.omit(airquality)
+
+
+
+# Exercise: using the tapply() command ------------------------------------
+
+# Calculate the average ozone value by month using the airquality dataset 
+# and the tapply command.
+
+tapply(airquality$Ozone,airquality$Month,mean,na.rm=T)
 
 
 # Exercise: Advanced Base Graphics ----------------------------------------
@@ -53,3 +98,12 @@ plot(cars,lab=c(25,25,25), cex.axis=.6)
 plot(cars, lab=c(20,10,6), cex.axis=.6, las=1)
 
 points(x=c(23,26), y=c(60,61), col="red")
+
+
+
+
+# Add on ------------------------------------------------------------------
+
+ames_df <- AmesHousing::make_ames()
+DT::datatable(ames_df)
+
